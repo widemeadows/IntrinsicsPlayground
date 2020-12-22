863ca39fb733a1755ce9ef8c970090d9b41f9312
Markus Mayer <widemeadows@gmail.com>
2020-12-22 23:20:25 +0100

    Add separate tests for sorting

--- test/IntrinsicsPlayground.Tests/SortTests.cs
+++ test/IntrinsicsPlayground.Tests/SortTests.cs
@@ -1,29 +1,41 @@
 ﻿using System;
 using System.Linq;
-using IntrinsicsPlayground.Misc.Sorting;
+using FluentAssertions;
 using Xunit;
 
 namespace IntrinsicsPlayground.Tests
 {
     public class SortTests
     {
-        [Fact]
-        public void AllSortingAlgorithms()
+        private readonly int[] _unsortedArray;
+        private readonly int[] _sortedArray;
+
+        public SortTests()
         {
-            var bubbleSort = typeof(BubbleSort);
             var rand = new Random();
-            var unsortedArray = Enumerable.Range(0, 1024).OrderBy(i => rand.Next()).ToArray();
-            var sortedArrayRef = unsortedArray.ToArray();
-            Array.Sort(sortedArrayRef);
+            _unsortedArray = Enumerable
+                .Range(0, 1024)
+                .OrderBy(i => rand.Next())
+                .ToArray();
 
-            foreach (var sortType in bubbleSort.Assembly.GetTypes().Where(t => t.Namespace == bubbleSort.Namespace))
-            {
-                var array = unsortedArray.ToArray();
-                var method = sortType.GetMethod("Sort", new[] {typeof(int[])});
-                method.Invoke(null, new object[]{ array });
+            _sortedArray = _unsortedArray.ToArray();
+            Array.Sort(_sortedArray);
+        }
 
-                Assert.True(array.SequenceEqual(sortedArrayRef));
-            }
+        [Fact]
+        public void BubbleSort()
+        {
+            var array = _unsortedArray.ToArray();
+            Misc.Sorting.BubbleSort.Sort(array);
+            array.Should().BeEquivalentTo(_sortedArray, options => options.WithStrictOrdering());
+        }
+
+        [Fact]
+        public void DualPivotQuicksort()
+        {
+            var array = _unsortedArray.ToArray();
+            Misc.Sorting.DualPivotQuicksort.Sort(array);
+            array.Should().BeEquivalentTo(_sortedArray, options => options.WithStrictOrdering());
         }
     }
 }
