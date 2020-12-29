620b055b25569f0777ed36e80b678a2ae8dc879a
Markus Mayer <widemeadows@gmail.com>
2020-12-29 03:38:24 +0100

    Add Min optimizations and Median-of-5

---
+++ src/IntrinsincsPlayground/Benchmarks/Medians/Medians.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
+
+namespace IntrinsicsPlayground.Benchmarks.Medians
+{
+    [DisassemblyDiagnoser]
+    public class Medians
+    {
+        private int[] _unsortedArray;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var rand = new Random();
+            _unsortedArray = Enumerable
+                .Range(0, 1024)
+                .OrderBy(i => rand.Next())
+                .ToArray();
+        }
+
+        [Benchmark(Baseline = true)]
+        [MethodImpl(MethodImplOptions.AggressiveOptimization)]
+        public float Median5_Soft()
+        {
+            var array = _unsortedArray;
+            return Misc.Median.Medians.Median5_Soft(array[0], array[1], array[2], array[3], array[4]);
+        }
+
+        [Benchmark]
+        [MethodImpl(MethodImplOptions.AggressiveOptimization)]
+        public float Median5_Soft2()
+        {
+            var array = _unsortedArray;
+            return Misc.Median.Medians.Median5_Soft2(array[0], array[1], array[2], array[3], array[4]);
+        }
+
+        [Benchmark]
+        [MethodImpl(MethodImplOptions.AggressiveOptimization)]
+        public float Median5_Relaxed2()
+        {
+            var array = _unsortedArray;
+            return Misc.Median.Medians.Median5_Relaxed2(array[0], array[1], array[2], array[3], array[4]);
+        }
+    }
+}
